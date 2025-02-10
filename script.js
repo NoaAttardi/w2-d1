7362332d95d6491f8abd8829d33034d97240cff3
NoaAttardi <attardi576@gmail.com>
2025-02-10 14:48:14 +0100

    js.day1

---
+++ script.js
@@ -0,0 +1,20 @@
+let swag "myName"
+let number= 12
+let number1= 20
+
+ number=number+number1 
+ console.log (number) =30
+
+ let x = 12
+ const swag "attardi"
+ let swag "ciao"
+
+let number4= 4 
+
+number4 - x 
+console.log (x) =8
+
+let finisch "name1"
+let finisch1 "name2"
+finisch "john"
+finisch1 "John"
